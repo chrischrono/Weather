de46a303b02bd20512c7286ecec6c6fd4e9a7c1a
Christian <chris_chrono@yahoo.com>
2019-04-21 18:33:26 +0700

    Add Forecast Model

---
+++ Weather/Model/Forecast.swift
@@ -0,0 +1,37 @@
+//
+//  Forecast.swift
+//  Weather
+//
+//  Created by Christian on 21/04/19.
+//  Copyright © 2019 Christian. All rights reserved.
+//
+
+import Foundation
+
+struct ForecastResponse: Codable {
+    let latitude: Double
+    let longitude: Double
+    let timezone: String
+    let currently: Forecast
+}
+
+
+struct Forecast: Codable {
+    let time: Int
+    let summary: String?
+    let icon: String?
+    let precipIntensity: Float?
+    let precipProbability: Float?
+    let temperature: Float?
+    let apparentTemperature: Float?
+    let dewPoint: Float?
+    let humidity: Float?
+    let pressure: Float?
+    let windSpeed: Float?
+    let windGust: Float?
+    let windBearing: Float?
+    let cloudCover: Float?
+    let uvIndex: Float?
+    let visibility: Float?
+    let ozone: Float?
+}
